25aff003e289254f5e934870e66584ce09fad528
Tristan <trisdooley@gmail.com>
2023-09-07 16:45:13 -0600

    Update Group_Sketch.ino
    updated with a more robust IR communications library so that writing to pins and reading from the IR receiver can happen without interference(mostly)

--- Group_Sketch/Group_Sketch.ino
+++ Group_Sketch/Group_Sketch.ino
@@ -3,9 +3,9 @@
 //	IR_Button_VolDown = Reverse
 //	IR_Button_Play = All Stop
 //	IR_Button_EQ = Servo
-//Include IR Remote Library by Ken Shirriff
-//hashtag is part of include library statement
-#include "TinyIRReceiver.hpp"
+#define IR_SMALLD_NEC         //1st: define which protocol to use:
+//#define IR_SMALLD_NECx      //     IR_SMALLD_NEC or IR_SMALLD_NECx, (not both);
+#include <IRsmallDecoder.h>   //2nd: include the library;
 #include <Servo.h>
 
 //Define mapped buttons
@@ -31,20 +31,26 @@
 #define IR_Button_8 25
 #define IR_Button_9 26
 
-#define RECV_PIN 12 //Define sensor pin
+#define RECV_PIN 2 //Define sensor pin
 
 #define FWD_PIN 8
 #define REV_PIN 7
 #define SPD_PIN 6
-#define Servo_PIN 11
+#define Servo_PIN 9
 
-int cmd = 0;
+IRsmallDecoder irDecoder(RECV_PIN);  //3rd: create one decoder object with the correct digital pin;
+irSmallD_t irData;            //4th: declare one decoder data structure;
+
+int timeout;
+long lastTime;
+int cmd = 1;
 int filtered_cmd = 1;
 Servo flag_servo;
+bool repeat;
+bool killed;
 
 void setup(){
   Serial.begin(9600); //Output to Serial Monitor @ 9600 Baud
-  IrReceiver.begin(RECV_PIN); //Enable IR Receiver
   
   pinMode(FWD_PIN, OUTPUT);
   pinMode(REV_PIN, OUTPUT);
@@ -54,47 +60,63 @@ void setup(){
 
 //Loops constantly during operation, shows information sent to serial monitor
 void loop(){ 
-  if(IrReceiver.decode()){
-    cmd = IrReceiver.decodedIRData.command;
-    if(cmd == 0 || cmd == 64) {} else {
+  if(irDecoder.dataAvailable(irData)){
+    Serial.print("decoded!");
+    killed = false;
+    cmd = irData.cmd;
+    repeat = irData.keyHeld;
+    lastTime = millis();
+    if(repeat) {
+      timeout = 200;
+    } else {
+      timeout = 600;
+    }
+    if(cmd == 0) {} else {
       filtered_cmd = cmd;
     } 
-  	IrReceiver.resume();
   }
-  if(IrReceiver.isIdle()){} else {
+
+  if(millis()-timeout > lastTime && !killed) {
+    filtered_cmd = 1;
+    killed = true;
+  }
+
   switch(filtered_cmd) {
     //Go forward
-    case IR_Button_VolUp:
+    case IR_Button_Forward:
     	digitalWrite(FWD_PIN, HIGH);
     	digitalWrite(REV_PIN, LOW);
     	analogWrite(SPD_PIN, 255);
       Serial.println("forward");
     	break;
     //Go backward
-    case IR_Button_VolDown:
+    case IR_Button_Back:
     	digitalWrite(FWD_PIN, LOW);
     	digitalWrite(REV_PIN, HIGH);
     	analogWrite(SPD_PIN, 255);
       Serial.println("reverse");
     	break;
     //All stop
-    case IR_Button_Power:
-    	analogWrite(SPD_PIN, 0);
-      Serial.println("stop");
-    	break;
+    // case IR_Button_Power:
+    // 	analogWrite(SPD_PIN, 0);
+    //   Serial.println("stop");
+    // 	break;
     //Servo
     case IR_Button_EQ:
-      Serial.println("servo start");
-      flag_servo.write(0);
-    	delay(1000);
+      Serial.println("servo 180");
       flag_servo.write(180);
-      Serial.println("servo end");
-      filtered_cmd = 1;
     	break;
+    case 1:
+      Serial.println("off");
+    	analogWrite(SPD_PIN, 0);
+      flag_servo.write(0);
+      filtered_cmd = 2;
+      break;
+    case 2:
+      break;
    	default:
    		Serial.print("Unbound button: ");
     	Serial.println(filtered_cmd);
-  }
   }
 }
 
